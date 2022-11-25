e0c5189ff25b2c9f1cd7564c2d05edb86962b6da
Patryk Orlowski <patryk.orlowski1992@gmail.com>
2022-11-25 09:25:51 +0100

    next small changes to css, fonts and overall look of my portfolio

--- src/js/jqueryscript.js
+++ src/js/jqueryscript.js
@@ -1,5 +1,8 @@
 $(document).ready(function () {
-    $("nav").click(function () {
-        $("header").fadeOut();
+    $("#down").click(function () {
+        $("#down").fadeOut(1000);
     });
+    $(window).scroll(function () {
+        $("#down").fadeOut(1000);
+    })
 });
