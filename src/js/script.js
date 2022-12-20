952bb94e604d136e8aac87b4dabe9e904b98bdbe
Patryk Orlowski <patryk.orlowski1992@gmail.com>
2022-12-20 01:30:29 +0100

    changes to starting page, small changes in js code, too much to desribe

--- src/js/script.js
+++ src/js/script.js
@@ -1,34 +1,43 @@
 "use strict";
-// Arrow down button
-const arrowDown = document.getElementById("down");
-// Creating list of links to projects
+// TScripts related to entering to the page
+const navLinks = document.querySelectorAll("nav a");
+const [welcomeDiv1, welcomeDiv2] = document.querySelectorAll(".welcome");
+
+const showPortfolioSite = function () {
+  welcomeDiv1.classList.add("welcome-view-hidden-1");
+  welcomeDiv2.classList.add("welcome-view-hidden-2");
+  setTimeout(() => {
+    navLinks.forEach((e) => {
+      e.classList.remove("hidden-nav-links");
+    });
+  }, 1500);
+};
+
+document
+  .querySelector(".welcome-view-1 > p")
+  .addEventListener("click", showPortfolioSite);
+///////
+
 const linksToProjects = document.querySelectorAll(".project-link");
 const linksToExitProjects = document.querySelectorAll(".exit-project");
-// Creating list of project divs
 const listOfProjects = document.querySelectorAll(".project");
+const listOfCenteredDivs = document.querySelectorAll(".centered");
 
 // Showing and hidding projects
-for (let i = 0; i < 4; i++) {
-  linksToProjects[i].addEventListener("click", function () {
-    listOfProjects[i].classList.toggle("hidden");
-    document.querySelector(".mask").classList.toggle("blurred");
-  });
-}
 
-for (let i = 0; i < 4; i++) {
-  linksToExitProjects[i].addEventListener("click", function () {
-    listOfProjects[i].classList.toggle("hidden");
-    document.querySelector(".mask").classList.toggle("blurred");
-  });
-}
+linksToProjects.forEach((e, i) =>
+  e.addEventListener("click", function () {
+    listOfProjects[i].classList.toggle("hidden-p");
+    listOfCenteredDivs[i].classList.toggle("blurred");
+  })
+);
 
-// Hide arrow after click or screen scroll
-arrowDown.addEventListener("click", function () {
-  arrowDown.classList.add("hidden");
-});
-window.addEventListener("scroll", function () {
-  arrowDown.classList.add("hidden");
-});
+linksToExitProjects.forEach((e, i) =>
+  e.addEventListener("click", function () {
+    listOfProjects[i].classList.toggle("hidden-p");
+    listOfCenteredDivs[i].classList.toggle("blurred");
+  })
+);
 
 // Reveal segments
 const allSegments = document.querySelectorAll(".segment");
@@ -55,37 +64,21 @@ allSegments.forEach(function (segment) {
 const imgTargets = document.querySelectorAll("img[data-src]");
 
 const imgObserver = new IntersectionObserver(
-  function (entries, observer) {
+  (entries, observer) => {
     const [entry] = entries;
-    if (!entry.isIntersecting) return;
-    entry.target.src = entry.target.dataset.src;
-    entry.target.addEventListener("load", function () {
-      entry.target.classList.remove("lazy-img");
-    });
-    observer.unobserve(entry.target);
+    if (entry.isIntersecting) {
+      entry.target.src = entry.target.dataset.src;
+      entry.target.addEventListener("load", function () {
+        entry.target.classList.remove("lazy-img");
+      });
+      observer.unobserve(entry.target);
+    }
   },
   {
     root: null,
-    threshold: [0, 0.3, 0.8],
+    threshold: [0, 0.3, 0.5, 0.8],
     rootMargin: "500px",
   }
 );
 
 imgTargets.forEach((target) => imgObserver.observe(target));
-
-// Troll site hide and prevent from leaving
-const trollsDivs = document.querySelectorAll(".troll");
-const trollEvent = trollsDivs.forEach((e) =>
-  e.addEventListener("click", function (e) {
-    console.log(trollsDivs);
-    trollsDivs[0].classList.add("troll-view-hidden-1");
-    trollsDivs[1].classList.add("troll-view-hidden-2");
-    removeEventListener("click", trollEvent, false);
-  })
-);
-
-setTimeout(() => {
-  trollsDivs[0].classList.add("troll-view-hidden-1");
-  trollsDivs[1].classList.add("troll-view-hidden-2");
-  removeEventListener("click", trollEvent, false);
-}, 1500);
